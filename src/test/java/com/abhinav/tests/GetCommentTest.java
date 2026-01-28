3c74d8de02cecc4d40cf89c541df341bcf5cc73b
ABHINAV KUMAR <56204649+abhi9avx@users.noreply.github.com>
2026-01-28 22:08:40 +0530

    refactor: make `GetCommentTest` extend `BaseTest` for better test structure.

--- src/test/java/com/abhinav/tests/GetCommentTest.java
+++ src/test/java/com/abhinav/tests/GetCommentTest.java
@@ -5,23 +5,23 @@ import com.abhinav.framework.dto.CommentResponseDto;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GetCommentTest {
+public class GetCommentTest extends BaseTest {
 
-  // Test the get comment API to ensure it returns the correct data.
-  // We verify that the ID, email, and content match exactly what we expect from
-  // the server.
-  @Test
-  public void testGetCommentById() {
+    // Test the get comment API to ensure it returns the correct data.
+    // We verify that the ID, email, and content match exactly what we expect from
+    // the server.
+    @Test
+    public void testGetCommentById() {
 
-    CommentResponseDto comment = CommentController.getCommentById();
+        CommentResponseDto comment = CommentController.getCommentById();
 
-    // Assert
-    Assert.assertEquals(comment.getId(), 1);
-    Assert.assertEquals(comment.getPostId(), 1);
-    Assert.assertEquals(comment.getName(), "id labore ex et quam laborum");
-    Assert.assertEquals(comment.getEmail(), "Eliseo@gardner.biz");
-    Assert.assertEquals(
-        comment.getBody(),
-        "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium");
-  }
+        // Assert
+        Assert.assertEquals(comment.getId(), 1);
+        Assert.assertEquals(comment.getPostId(), 1);
+        Assert.assertEquals(comment.getName(), "id labore ex et quam laborum");
+        Assert.assertEquals(comment.getEmail(), "Eliseo@gardner.biz");
+        Assert.assertEquals(
+                comment.getBody(),
+                "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium");
+    }
 }
