c9a0c1d6235f7e66cda7e37dadf39346b5a04580
ABHINAV KUMAR <56204649+abhi9avx@users.noreply.github.com>
2026-01-28 21:55:16 +0530

    feat: Implement API controller, DTO, and enum for fetching comments by ID and add a corresponding test.

---
+++ src/main/java/com/abhinav/framework/dto/CommentResponseDto.java
@@ -0,0 +1,25 @@
+package com.abhinav.framework.dto;
+
+import lombok.*;
+
+// This class is a Data Transfer Object (DTO) to map the API response.
+// It helps us easily access comment data using Java objects instead of parsing JSON manually.
+@Data
+public class CommentResponseDto {
+
+    private Integer postId;
+    private Integer id;
+    private String name;
+    private String email;
+    private String body;
+}
+
+// {
+// "postId": 1,
+// "id": 1,
+// "name": "id labore ex et quam laborum",
+// "email": "Eliseo@gardner.biz",
+// "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora
+// quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente
+// accusantium"
+// }
---
+++ src/test/java/com/abhinav/tests/GetCommentTest.java
@@ -0,0 +1,28 @@
+package com.abhinav.tests;
+
+import org.testng.annotations.Test;
+import com.abhinav.framework.controller.CommentController;
+import com.abhinav.framework.dto.CommentResponseDto;
+import org.testng.Assert;
+
+public class GetCommentTest {
+
+    // Test the get comment API to ensure it returns the correct data.
+    // We verify that the ID, email, and content match exactly what we expect from
+    // the server.
+    @Test
+    public void testGetCommentById() {
+
+        CommentResponseDto comment = CommentController.getCommentById();
+
+        // Assert
+        Assert.assertEquals(comment.getId(), 1);
+        Assert.assertEquals(comment.getPostId(), 1);
+        Assert.assertEquals(comment.getName(), "id labore ex et quam laborum");
+        Assert.assertEquals(comment.getEmail(), "Eliseo@gardner.biz");
+        Assert.assertEquals(comment.getBody(),
+                "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium");
+
+    }
+
+}
