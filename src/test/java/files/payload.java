788178de63fc357ffbe7070c17889cd9a3f8d420
ABHINAV KUMAR <56204649+abhi9avx@users.noreply.github.com>
2026-02-14 11:03:08 +0530

    feat: Add utility for parsing and validating complex JSON structures for course pricing.

---
+++ src/test/java/files/payload.java
@@ -0,0 +1,30 @@
+package files;
+
+public class payload {
+
+    public static String CoursePrice() {
+        return "{\n" +
+                "  \"dashboard\": {\n" +
+                "    \"purchaseAmount\": 1510,\n" +
+                "    \"website\": \"rahulshettyacademy.com\"\n" +
+                "  },\n" +
+                "  \"courses\": [\n" +
+                "    {\n" +
+                "      \"title\": \"Selenium Python\",\n" +
+                "      \"price\": 150,\n" + //
+                "      \"copies\": 6\n" + //
+                "    },\n" + //
+                "    {\n" + //
+                "      \"title\": \"Cypress\",\n" + //
+                "      \"price\": 40,\n" + //
+                "      \"copies\": 4\n" + //
+                "    },\n" + //
+                "    {\n" + //
+                "      \"title\": \"RPA\",\n" + //
+                "      \"price\": 45,\n" + //
+                "      \"copies\": 10\n" + //
+                "    }\n" + //
+                "  ]\n" + //
+                "}\n";
+    }
+}
