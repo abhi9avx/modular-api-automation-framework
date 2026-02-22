74ab333c599bcf845010d4cfdb3afea62688d34b
ABHINAV KUMAR <56204649+abhi9avx@users.noreply.github.com>
2026-02-22 11:31:57 +0530

    complex payload with object

---
+++ src/main/java/com/abhinav/framework/dto/RootRequest.java
@@ -0,0 +1,85 @@
+package com.abhinav.framework.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RootRequest {
+    private User user;
+    private Project project;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class User {
+        private int id;
+        private String name;
+        private List<String> roles;
+        private Profile profile;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Profile {
+        private String email;
+        private List<Phone> phones;
+        private Address address;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Phone {
+        private String type;
+        private String number;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Address {
+        private String street;
+        private String city;
+        private Geo geo;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Geo {
+        private double lat;
+        private double lng;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Project {
+        private String name;
+        private List<Module> modules;
+        private boolean active;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Module {
+        private String module;
+        private String status;
+    }
+}
